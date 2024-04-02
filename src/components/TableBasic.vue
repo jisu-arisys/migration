c63eae8384a228fd8879792c18fe3d400116fc63
jisu-arisys <jisu_um@arisys.co.kr>
2024-04-02 13:25:43 +0900

    add tableBasic

---
+++ src/components/TableBasic.vue
@@ -0,0 +1,40 @@
+<template>
+  <table class="table">
+    <thead>
+      <slot name="columns">
+        <tr>
+          <th v-for="column in columns" :key="column">{{column}}</th>
+        </tr>
+      </slot>
+    </thead>
+    <tbody>
+    <tr v-for="(item, index) in data" :key="index">
+      <slot :row="item" v-if="hasValue(item, column)">
+        <td v-for="column in columns" :key="column" @click="handleColumnClick(item)">{{itemValue(item, column)}}</td>
+      </slot>
+    </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+  export default {
+    name: 'l-table',
+    props: {
+      columns: Array,
+      data: Array
+    },
+    methods: {
+      hasValue (item, column) {
+        return item[column.toLowerCase()] !== 'undefined'
+      },
+      itemValue (item, column) {
+        return item[column.toLowerCase()]
+      },
+      handleColumnClick(item) {
+      this.$emit('handle', item);
+      }
+    }
+  }
+</script>
+<style>
+</style>
