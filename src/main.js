bfa9eb5f93c3f9cdc5d8581c87e4f92ba7cd23c4
jisu-arisys <jisu_um@arisys.co.kr>
2024-04-03 09:32:44 +0900

    add card globalComponent

--- src/main.js
+++ src/main.js
@@ -2,20 +2,21 @@ import { createApp, ref  } from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './router'
 
 // new
 import './assets/sass/light-bootstrap-dashboard.scss' //need 'sass-loader'
 import './assets/css/demo.css'
 
-import dayjs from "dayjs";
+import dayjs from "dayjs"
 import createCalculator from "./module/dateCalculator" //dayjs : 사용자 정의 계산식 함수가 포함된 .js 호출
 import createfilters from "./module/filters" //사용자정의 테이블 옵션, 필터링, 페이징 함수가 포함된 .js 호출
 
-import router from './router'
+import Card from './globalComponent/Card.vue'
 
-const app = createApp(App);
-
-app.use(router);
+const app = createApp(App)
+app.use(router)
+app.component('Card', Card)
 
 // sidebarStore를 ref로 반응성 객체를 정의합니다.
 const sidebarStore = ref({
@@ -27,7 +28,7 @@ const sidebarStore = ref({
   displaySidebar(value) {
     this.showSidebar = value;
   }
-});
+})
 
 // app.config.globalProperties에 $sidebar를 설정합니다.
 app.config.globalProperties.$sidebar = {
@@ -40,10 +41,12 @@ app.config.globalProperties.$sidebar = {
   displaySidebar(value) {
     sidebarStore.value.displaySidebar(value);
   }
-};
+}
 
-app.config.globalProperties.$dayjs = dayjs;
-app.config.globalProperties.$cal = createCalculator(dayjs);
-app.config.globalProperties.$filter = createfilters();
+app.config.globalProperties.$dayjs = dayjs
+app.config.globalProperties.$cal = createCalculator(dayjs)
+app.config.globalProperties.$filter = createfilters()
 
-app.mount('#app');
+
+
+app.mount('#app')
